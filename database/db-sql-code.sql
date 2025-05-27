59c0b9f7910b4a0efc4ce9177aca2f44184061a7
BlandineB <blandinebukasa118@gmail.com>
2025-05-27 13:24:29 +0200

    Update

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -6,7 +6,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin ');
 
 ALTER TYPE public.account_type
-    OWNER TO blandib;
+    OWNER TO cse340br;
 
 -- Table structure for table `classification`
 CREATE TABLE public.classification (
